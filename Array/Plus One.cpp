238fd4f2faaad4b9bd3f1403b29fec900bf4a0f4
Amit Kumar <amitk200415@gmail.com>
2025-05-14 01:30:26 +0530

    .

---
+++ Array/Plus One.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/plus-one/description/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // If we reached here, all digits were 9
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
